efe6fec413142b743532aa490ed54c39424fc797
Jingjing <jjluo2007@gmail.com>
2022-07-18 15:57:05 -0400

    Loading Practice Book Dataset to R

--- loadingData.R
+++ loadingData.R
@@ -1,22 +1,12 @@
-#### Loading in csv file ####
-
-books_dataset <- read.csv("data/books.csv") #reads the csv file,
-View(books_dataset)
+#### load in required libraries ####
 library(ggplot2)
 
-## Practice plotting with your dataset
 
-#can't subset the num_pages bc is calculating the whole dataset
 
-ggplot(data = books_dataset, aes(x = as.numeric(average_rating))) +
-  geom_histogram(binwidth = 0.25) 
+#### Load in csv file ####
+books_dataset <- read.csv("data/books.csv") #reads the csv file,
+View(books_dataset)
 
-ggplot(data = books_dataset, aes(x = average_rating)) +
-  labs(x = "Average Rating")
-
-ggplot(data = books_dataset, aes(x = ratings_count, y = as.numeric(average_rating))) +
-  geom_line(stat = "summary",
-            fun = "mean")
-  
-
+#### Save R object as file to github ####
+saveRDS(books_dataset, "data/books_dataset.RDS") #compress dataset to make smaller than 5 mg
 
